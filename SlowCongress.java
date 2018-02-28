a65cc895998a8ecab915ee41c22423203f6c61c0
Henriette <Hlaursen86@gmail.com>
2018-02-28 18:36:28 +0100

    Code works on Codejudge except that it is too slow to run one of the huge tests.

--- SlowCongress.java
+++ SlowCongress.java
@@ -8,46 +8,45 @@ public class SlowCongress {
   private int numberOfStates;
 
   public SlowCongress(int totalSeatNumber, int numberOfStates, double[] priorities, String[] stateNames ) {
+
     this.priorityArray = priorities;
     this.statesArray = stateNames;
     this.seatsArray = new int[numberOfStates];
     this.seatsLeft = totalSeatNumber;
     this.numberOfStates = numberOfStates;
 
-    //Initially give each state 1 seat if there are enough seats - if there are not enough seats, the seats have to be assigned to the bigger states - this is not handled right now
+    //Initially give each state 1 seat if there are enough seats - there will always be enough seats for the first round (definition of Huntington-Hill rule)
     for (int i = 0; i<numberOfStates; i++) {
-      if (seatsLeft != 0) {
-          seatsArray[i] = 1;
-          seatsLeft--;
-          //priorityArray[i] = calculatePriority(priorityArray[i], seatsArray[i]);
-      } else {
-        seatsArray[i] = 0;
-      }
-    }
-
-    //Calculate quotient for all states
-    for (int i = 0; i<numberOfStates; i++) {
+      seatsArray[i] = 1;
+      seatsLeft--;
       priorityArray[i] = calculatePriority(priorityArray[i], seatsArray[i]);
-      StdOut.println("State: " + statesArray[i] + "Priority: " + priorityArray[i]);
+      //StdOut.println("State: " + statesArray[i] + " Priority: " + priorityArray[i]);
     }
-
   }
 
   public double calculatePriority(double currentPriority, int currentSeats) {
     double newPriority;
-    newPriority = currentPriority/Math.sqrt(currentSeats*(currentSeats + 1));
-    //StdOut.println(newPriority);
+    double calculatedOriginal; // Calculates the initial population so this does not have to be stored in an array - this might not be the most efficient way
+
+    if (currentSeats > 1) {
+      calculatedOriginal = currentPriority * Math.sqrt((currentSeats-1)*(currentSeats));
+    }
+    else { //if it is the first seat received --> currentPriority has the original population
+      calculatedOriginal = currentPriority;
+    }
+
+    //newPriority = currentPriority/Math.sqrt(currentSeats*(currentSeats + 1)); //Go back to this if we decide to store the original population in an array
+    newPriority = calculatedOriginal/Math.sqrt(currentSeats*(currentSeats + 1));
     return newPriority;
   }
 
   public void giveSeat() {
-    //Gives a seat to the state with highest priority
+    //Gives a seat to the state with highest priority but find index it first
     double max = 0;
     int maxIndex = 0;
 
-    // Find higest priority
+    // Find highest priority
     if (this.seatsLeft!=0) {
-      //Find state with highest priority
       for (int j = 0; j < numberOfStates; j++) {
         if (priorityArray[j] > max) {
           max = priorityArray[j];
